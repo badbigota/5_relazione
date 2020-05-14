b892c096dcdb933b1d7f09cbd1fb0f851242dc5a
Tommaso Bertola <bertola.tommaso@hotmail.it>
2020-05-14 11:06:15 +0200

    Add calcolo tempi zero

--- Programmi/analisi.cxx
+++ Programmi/analisi.cxx
@@ -8,13 +8,17 @@ using namespace std;
 
 int main()
 {
-    vector<data_torque> tor;
+    vector<data_torque> torq;
+    vector<x_y> periodi;
     string map_file = "../Dati/mappa.txt";
-    load_data(map_file, tor);
-
+    load_data(map_file, torq);
+    get_zero_time(torq, periodi);
     
-    for (auto d : tor)//controllo per i dati
+    for (auto d : torq)//controllo per i dati
     {
         cout << d.time.size() << "\t" << d.forcing.size() << "\t" << d.a.size() << endl;
+    }
+    for(auto d: periodi){
+        cout<<d.time.size()<<endl;
     }
 }
