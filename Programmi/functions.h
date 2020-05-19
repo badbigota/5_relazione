53bc716469f49dcf73ba96f2fba67ba52f7f4534
Tommaso Bertola <bertola.tommaso@hotmail.it>
2020-05-20 01:08:55 +0200

    Correzione trova massimi, add picco picco mv e ass, offset, calcolo omega

--- Programmi/functions.h
+++ Programmi/functions.h
@@ -32,24 +32,41 @@ struct x_y
     vector<double> time;
     vector<double> amplitude;
     vector<double> omega_sperim;
+    vector<double> picco_picco;
     double omega_media1;
     double omega_media2;
     double err_omega_media1;
     double err_omega_media2;
+    double picco_medio;
+    double err_picco_medio;
+    vector<double> offset;
 };
 
 struct punti_massimo
 {
+    double freq_ref;
     vector<double> t_max;
     vector<double> ampl_max;
     vector<double> index; //per una veloce referenza ai dati grezzi
     vector<double> t_ref; //idem sopra
+    vector<double> a_ref;
+    vector<double> coeff_a;
+    vector<double> coeff_b;
+    vector<double> coeff_c;
 };
 
 struct vettoredoppio
 {
     vector<double> vettore2; //per l'indice del massimo
     vector<double> vettore3; //per il valore del massimo
+};
+
+struct punto_regime //perchè useremo questa per avere tutti i puti per il grafico
+{
+    double omega;
+    double err_omega;
+    double theta;
+    double err_theta;
 };
 
 //Carica tutti i dati grezzi in strutture contenute in un vettore
@@ -69,8 +86,8 @@ void load_data(string map_file, vector<data_campione> &vec_data)
         while (f_data_points >> time >> forcing >> a)
         {
             temp.time.push_back(time);
-            temp.a.push_back(a);
-            temp.forcing.push_back(forcing);
+            temp.a.push_back(a * 2. * M_PI);
+            temp.forcing.push_back(forcing * 1000. * 2. * M_PI);
         }
         vec_data.push_back(temp);
     }
@@ -181,6 +198,7 @@ void get_periodi_medi(vector<x_y> &periodi, vector<x_y> &media_periodi) //genra 
 }
 
 //Trova gli indici di massimo in assoluto, utili anche poi per fare picco picco automaticamente senza approx
+//Adattato interamente da quello di marc e fab in analisi cxx
 void get_index_maxima(vector<data_campione> &dati_grezzi, vector<x_y> &tempi, vector<vettoredoppio> &indici_max_per_campioni)
 {
     for (int i = 0; i < dati_grezzi.size(); i++) //per ogni cmapione
@@ -196,12 +214,11 @@ void get_index_maxima(vector<data_campione> &dati_grezzi, vector<x_y> &tempi, ve
             {
                 if (abs(ampiezze_grezze[num]) > val_max)
                 {
-                    val_max = ampiezze_grezze[num];
+                    val_max = abs(ampiezze_grezze[num]);
                     index_max = num;
                 }
             }
             massimi.vettore2.push_back(index_max);
-            massimi.vettore3.push_back(val_max);
         }
         indici_max_per_campioni.push_back(massimi);
     }
@@ -240,10 +257,10 @@ void best_fit_mv(vector<double> x, vector<double> y, vector<double> &best_fit)
     f = sum_pow_special(x, y, 2, 1);
     g = sum_pow_special(x, y, 1, 1);
     h = sum_pow_special(x, y, 0, 1);
-    double num_a = -pow(d, 2) * f + c * e * f + c * d * g - b * e * g - pow(c, 2) * h + b * d * h;
-    double num_b = c * d * f - b * e * f - pow(c, 2) * g + a * e * g + b * c * h - a * d * h;
-    double num_c = -pow(c, 2) * f + b * d * f + b * c * g - a * d * g - pow(b, 2) * h + a * c * h;
-    double den = pow(c, 3) - 2 * b * c * d + a * pow(d, 2) + pow(b, 2) * e - a * c * e;
+    long double num_a = -pow(d, 2) * f + c * e * f + c * d * g - b * e * g - pow(c, 2) * h + b * d * h;
+    long double num_b = c * d * f - b * e * f - pow(c, 2) * g + a * e * g + b * c * h - a * d * h;
+    long double num_c = -pow(c, 2) * f + b * d * f + b * c * g - a * d * g - pow(b, 2) * h + a * c * h;
+    long double den = pow(c, 3) - 2 * b * c * d + a * pow(d, 2) + pow(b, 2) * e - a * c * e;
     best_fit.push_back(-num_a / den);
     best_fit.push_back(-num_b / den);
     best_fit.push_back(-num_c / den);
@@ -252,21 +269,21 @@ void best_fit_mv(vector<double> x, vector<double> y, vector<double> &best_fit)
 ////Punto di massimo per fit
 double max_x_fit(vector<double> parametri_fit)
 {
-    double a = parametri_fit[0];
-    double b = parametri_fit[1];
-    double c = parametri_fit[2];
-    double max_val = -b / (2 * a);
+    long double a = parametri_fit[0];
+    long double b = parametri_fit[1];
+    long double c = parametri_fit[2];
+    long double max_val = -b / (2 * a);
     return max_val;
 }
 
 //////Valore punto di massimo per fit
 double max_y_fit(vector<double> parametri_fit)
 {
-    double a = parametri_fit[0];
-    double b = parametri_fit[1];
-    double c = parametri_fit[2];
-    double max_val = -b / (2 * a);
-    double y_max = a * pow(max_val, 2) + b * max_val + c;
+    long double a = parametri_fit[0];
+    long double b = parametri_fit[1];
+    long double c = parametri_fit[2];
+    long double max_val = -b / (2 * a);
+    long double y_max = a * pow(max_val, 2) + b * max_val + c;
     return y_max;
 }
 
@@ -294,7 +311,92 @@ void get_maxima(vector<data_campione> &raw_data, vector<vettoredoppio> &index_ma
             temp_punti_max.ampl_max.push_back(max_y_fit(best_fitting_par)); //trova valore punto di max
             temp_punti_max.index.push_back(pivot);                          //per un traceback accurato
             temp_punti_max.t_ref.push_back(tempi_grezzi[pivot]);            //per un traceback accurato
+            temp_punti_max.a_ref.push_back(ampiezze_grezze[pivot]);
+            temp_punti_max.freq_ref = raw_data[i].data_file_freq; //per un traceback
+            temp_punti_max.coeff_a.push_back(best_fitting_par[0]);
+            temp_punti_max.coeff_b.push_back(best_fitting_par[1]);
+            temp_punti_max.coeff_c.push_back(best_fitting_par[2]);
         }
         maxima.push_back(temp_punti_max);
+    }
+}
+
+void offset(vector<punti_massimo> vec_punti_max, vector<x_y> &vec_picco_picco)
+{
+    for (int i = 0; i < vec_punti_max.size(); i++)
+    {                                                          //per ogni campione
+        vector<double> &ampiezze_max = vec_punti_max[i].a_ref; //su i punti grezzi
+        x_y temp_picchi;
+        for (int j = 0; j < ampiezze_max.size() - 1; j = j + 2) //altirmenti core dumped
+        {
+            double offset = (ampiezze_max[j] + ampiezze_max[j + 1]) / 2.;
+            temp_picchi.offset.push_back(offset);
+        }
+        temp_picchi.freq = vec_punti_max[i].freq_ref;
+        vec_picco_picco.push_back(temp_picchi);
+    }
+}
+
+//Picco picco con valori massimi assoluti
+void picco_picco_ass(vector<punti_massimo> vec_punti_max, vector<x_y> &vec_picco_picco)
+{
+    for (int i = 0; i < vec_punti_max.size(); i++)
+    {                                                          //per ogni campione
+        vector<double> &ampiezze_max = vec_punti_max[i].a_ref; //su i punti grezzi
+        x_y temp_picchi;
+        for (int j = 0; j < ampiezze_max.size() - 1; j = j + 2) //altirmenti core dumped
+        {
+            double picco_picco_ = abs(ampiezze_max[j] - ampiezze_max[j + 1]);
+            temp_picchi.picco_picco.push_back(picco_picco_ / 2.);
+        }
+        temp_picchi.freq = vec_punti_max[i].freq_ref;
+        vec_picco_picco.push_back(temp_picchi);
+    }
+}
+
+//Picco picco a partire da massima verosimiglianza
+void picco_picco_mv(vector<punti_massimo> vec_punti_max, vector<x_y> &vec_picco_picco)
+{
+    for (int i = 0; i < vec_punti_max.size(); i++)
+    {                                                             //per ogni campione
+        vector<double> &ampiezze_max = vec_punti_max[i].ampl_max; //su i punti grezzi
+        x_y temp_picchi;
+        for (int j = 0; j < ampiezze_max.size() - 1; j = j + 2) //altirmenti core dumped
+        {
+            double picco_picco_ = abs(ampiezze_max[j] - ampiezze_max[j + 1]);
+            temp_picchi.picco_picco.push_back(picco_picco_ / 2.);
+        }
+        temp_picchi.freq = vec_punti_max[i].freq_ref;
+        vec_picco_picco.push_back(temp_picchi);
+    }
+}
+
+void picco_picco_chi() {}
+
+//Calcola la media e la dstd dei picco picco mezzi
+void add_picco_medio(vector<x_y> picchi, vector<punto_regime> &punti_regime)
+{
+    punto_regime temp_punto_reg;
+    for (auto d : picchi)
+    {
+        temp_punto_reg.theta = media(d.picco_picco, 0, 20);
+        temp_punto_reg.err_theta = dstd_media(d.picco_picco, 0, 20);
+        punti_regime.push_back(temp_punto_reg);
+    }
+}
+
+//Calcola gli omega secondo JCGM. METTERE SOLO DOPO CALCOLO DI THETA
+void add_omega_2(vector<x_y> periodi, vector<punto_regime> &punti_regime)
+{
+    for (int i = 0; i < periodi.size(); i++)
+    {
+        vector<double> temp_omega;
+        for (auto d : periodi[i].time)
+        {
+            temp_omega.push_back(2. * M_PI / d);
+        }
+
+        punti_regime[i].omega = media(temp_omega);
+        punti_regime[i].err_omega = dstd_media(temp_omega);
     }
 }
